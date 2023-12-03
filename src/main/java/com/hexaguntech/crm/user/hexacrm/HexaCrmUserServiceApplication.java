ef3f640ab232d2bc92e131e8621ef2c26531c365
ranjithkumaru <ranjithkumaru@purchasingpower.com>
2023-12-03 23:43:01 +0530

    IC

---
+++ src/main/java/com/hexaguntech/crm/user/hexacrm/HexaCrmUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hexaguntech.crm.user.hexacrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaCrmUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexaCrmUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hexaguntech/crm/user/hexacrm/HexaCrmUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexaguntech.crm.user.hexacrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaCrmUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
